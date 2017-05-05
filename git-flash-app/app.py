a74903bc208e7e3316c0bf9ff3903f8f2290b687
Son Thai <thaison242@gmail.com>
2017-05-04 21:52:27 -0700

    Assignment 1

--- git-flash-app/app.py
+++ git-flash-app/app.py
@@ -6,7 +6,7 @@ import json
 import urllib2
 
 app = Flask(__name__)
-
+git_repo = sys.argv[1]
 @app.route("/v1/<filename>")
 def get_content(filename):
     url = get_url(filename)
@@ -22,11 +22,11 @@ def get_file_content(url):
         return "Failed to get latest commits."
 
 def get_url(filename):
-    git_repo = app.config.get('git_repo')
+    #git_repo = app.config.get('git_repo')
     m = re.search('https://github.com/(.*)', git_repo)
     url = 'https://api.github.com/repos/' + m.group(1) + '/contents/' + filename
     return url
 
 if __name__ == "__main__":
-    app.config['git_repo'] = sys.argv[1]
+    #app.config['git_repo'] = sys.argv[1]
     app.run(debug=True, host='0.0.0.0')
